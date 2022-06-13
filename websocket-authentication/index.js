66765520b0a321aee2b243d2cbdc3f4086518b24
RikThePixel <63408969+Rikthepixel@users.noreply.github.com>
2022-06-13 19:34:22 +0200

    make working auth implementation

--- websocket-authentication/index.js
+++ websocket-authentication/index.js
@@ -1,38 +1,37 @@
 const express = require("express");
 const http = require("http");
 const ws = require("ws");
+
+//Some class that handles token creation, validation, encryption and decryption
 const { AuthService } = require("./authService");
 
 const app = express();
 const server = http.createServer(app);
 const wsApp = new ws.Server({ noServer: true });
-
 const authService = new AuthService();
 
 app.post("/login",
     (req, res) => res.send(authService.createToken(req.username, 60000))
 );
 
+app.on("upgrade", () => {
+
+});
+
 server.on('upgrade', (request, socket, head) => {
     const authHeader = request.headers.authorization;
-    if (!authService.isValid(authHeader)) {
-        return socket.destroy();
-    }
+    if (!authService.isValid(authHeader)) return socket.destroy();
 
     wsApp.handleUpgrade(
-        request,
-        socket,
-        head,
+        request, socket, head,
         (ws) => wsApp.emit('connection', ws, request)
     );
 });
 
-const sendAuthMessage = (socket) => {
-    socket.send(JSON.stringify({
-        type: "reauthenticate",
-        timestamp: Date.now()
-    }));
-};
+const sendJSON = (socket, data) => socket.send(JSON.stringify(data));
+const sendAuthMessage = (socket) => sendJSON(socket, {
+    type: "reauthenticate", timestamp: Date.now()
+});
 
 const refreshAuthState = (authState, token) => {
     const tokenData = authService.getTokenData(token);
@@ -40,29 +39,32 @@ const refreshAuthState = (authState, token) => {
     authState.experationDate = tokenData.experationDate;
 };
 
+const handleReauthMessage = (socket, authState, newToken) => {
+    if (!authService.isValid(newToken)) return sendAuthMessage(socket);
+    refreshAuthState(authState, newToken);
+};
+
 wsApp.on("connection", (socket, request) => {
+
     const authState = { username: null, experationDate: 0 };
     refreshAuthState(authState, request.headers.authorization);
 
     socket.on("message", (rawData) => {
         const msgData = JSON.parse(rawData.toString());
 
-        if (msgData.type === "reauthenticate") {
-            const newToken = msgData.payload;
-            if (!authService.isValid(newToken)) return sendAuthMessage(socket);
-            return refreshAuthState(authState, newToken);
-        }
+        //Handle reauthentication
+        if (msgData.type === "reauthenticate")
+            return handleReauthMessage(socket, authState, msgData.payload);
 
         //Handle authentication
         const isValid = authService.isDateValid(authState.experationDate);
         if (!isValid) return sendAuthMessage(socket);
 
-        // Handle message
-        console.log(msgData.payload);
-        socket.send(JSON.stringify({
+        //Handle message
+        sendJSON(socket, {
             type: "hello",
             payload: "Hello client"
-        }));
+        });
     });
 
 });
