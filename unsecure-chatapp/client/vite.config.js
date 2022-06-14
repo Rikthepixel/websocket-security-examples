1d0c0a49137bde8d496b399e162a70222b9e23ab
RikThePixel <63408969+Rikthepixel@users.noreply.github.com>
2022-06-14 08:53:51 +0200

    Set-up for dist

--- unsecure-chatapp/client/vite.config.js
+++ unsecure-chatapp/client/vite.config.js
@@ -4,7 +4,8 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
     build: {
         outDir: "../dist/client",
-        emptyOutDir: true
+        emptyOutDir: true,
+        assetsDir: "public/assets"
     },
 
     plugins: [react()]
--- unsecure-chatapp/server/index.ts
+++ unsecure-chatapp/server/index.ts
@@ -1,3 +1,16 @@
-// import Express from 'express';
-// const app = Express();
-console.log("Hello server");
+import Express from 'express';
+import path from 'path';
+const app = Express();
+const clientPath = path.join(__dirname, "../client");
+
+app.use("/public", Express.static(
+    path.join(clientPath, "/public")
+));
+
+app.get("*", (req, res) => {
+    return res.sendFile(
+        path.join(clientPath, "/index.html"),
+    );
+});
+
+app.listen(3001, () => console.log("App started on http://localhost:3001"));
